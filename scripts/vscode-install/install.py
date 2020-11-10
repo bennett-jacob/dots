cff40a1c651d9e1745889c076af4304f0b6b42f6
Jacob Bennett <bennettjacobk@gmail.com>
2020-11-10 08:06:48 -0700

    add logging

--- scripts/vscode-install/install.py
+++ scripts/vscode-install/install.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 from shutil import copyfile
+import subprocess
+from typing import Callable
 
 home_dir = Path(os.environ.get("HOME", "/root"))
 cwd = Path(__file__).parent.absolute()
@@ -20,9 +22,7 @@ def copy_file(src_path: Path, dest_path: Path):
     copyfile(src_path, dest_path)
 
 
-def install():
-    print("Installing bash configuration in VS Code")
-
+def install_config_files():
     files_to_be_copied = [
         root_dir.joinpath(".bashrc"),
         root_dir.joinpath(".hyper.js"),
@@ -34,6 +34,28 @@ def install():
         dest_file = home_dir.joinpath(src_file.name)
         copy_file(src_file, dest_file)
 
+
+def install_oh_my_bash():
+    subprocess.run([
+        "bash",
+        "-c",
+        '"$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"',
+    ])
+
+
+def do(func: Callable):
+    """Run func and print some formatting around it."""
+    print("--------------------------------------------------")
+    print(f"Running {func.__name__}")
+    print()
+    func()
+    print()
+
+
+def install():
+    print("Installing bash configuration in VS Code")
+    do(install_config_files)
+    do(install_oh_my_bash)
     print("Installation complete!")
 
 
