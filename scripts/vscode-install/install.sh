04437395343b22b5e8ced25a99c9a9065ec8ede8
Jacob Bennett <13636529+bennett-jacob@users.noreply.github.com>
2021-04-08 07:44:13 -0600

    remove scripts

--- scripts/vscode-install/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# ------------------------------------------------------------------------------
-# This is the installation script when installing this in VS Code devontainers.
-# ------------------------------------------------------------------------------
-
-python $HOME/dotfiles/scripts/vscode-install/install.py
